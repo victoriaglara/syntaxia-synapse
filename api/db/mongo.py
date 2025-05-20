6083649c352fae001145ff12efc17a90da8ddcf5
Victoria <victoriaglara@gmail.com>
2025-05-19 21:55:11 -0300

    fix: usa localhost como host do MongoDB no ambiente de testes CI

--- api/db/mongo.py
+++ api/db/mongo.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://mongo:27017")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 client = AsyncIOMotorClient(MONGO_URL)
 db = client["syntaxia"]
 
